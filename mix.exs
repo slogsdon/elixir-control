857aa108042fd5aec7ac374ee2b6608a81d1c5e6
Shane Logsdon <shane@shanelogsdon.com>
2015-10-04 09:11:40 -0400

    add hex information

--- mix.exs
+++ mix.exs
@@ -7,26 +7,29 @@ defmodule Control.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    An exploratory look into functors, applicatives, and monads for Elixir.
+    """
+  end
+
+  defp package do
+    [
+     maintainers: ["Shane Logsdon"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/slogsdon/elixir-control"}]
   end
 end
